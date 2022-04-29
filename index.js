6ee26ca63517ca5ccc821e7280ea8543639e4454
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2022-04-29 20:09:39 -0300

    Criado toda a base do formulario de tópico, exceto o  botão para enviar o formulário, que ta com um bug

---
+++ index.js
@@ -0,0 +1,13 @@
+function newTopic(){
+
+let x = document.getElementById("topico")
+x.innerHTML=""
+
+let nTopic = document.createElement('div')
+
+nTopic.innerHTML = "Roger"
+
+x.appendChild(nTopic)
+
+
+}
