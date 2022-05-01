8c91c4a3cf22d0e7e8a52ace7dbeadd832dd8c8f
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2022-05-01 10:54:36 -0300

    sexto dias de trabalho, projeto parcialmente finalizado, so falta responsividade e limpar um pouco o código

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var formulario = document.getElementById("criandoTopico")
-var topico = document.getElementById("topico")
-var confirmTopico = document.getElementById("confirmEnvioTopico")
+var formulario    =  document.getElementById("criandoTopico")
+var topico        =  document.getElementById("topico")
+var confirmTopico =  document.getElementById("confirmEnvioTopico")
+var showComents   =  document.getElementById("comentEscondidos")
 
 var textoNone1 = document.querySelector(".textoEscondido1");
 var textoNone2 = document.querySelector(".textoEscondido2");
@@ -50,5 +51,12 @@ function resumoInteiro (){
     textoNone1.style.display = "block"
     textoNone2.style.display = "block"
     textoNone3.style.display = "block"
+
+}
+
+function mostrarComents(){
+
+    showComents.style.display="block"
+
 
 }
